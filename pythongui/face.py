a4851e33471bafd9cf293b9c84d20a1bc7cfb85d
Devananth V <ep20btech11004@iith.ac.in>
2023-07-25 09:52:05 +0530

    first

---
+++ pythongui/face.py
@@ -0,0 +1,29 @@
+import PySimpleGUI as sg
+import cv2
+
+layout = [[sg.Image(key = '-IMAGE-')],
+          [sg.Push(),sg.Text('No of Faces Detected:'),sg.Text("0", key = '-TEXT-', expand_x = True)]]
+
+window = sg.Window('Face Detector', layout)
+
+video = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+while True:
+    event, values = window.read(timeout = 0)
+    if event == sg.WIN_CLOSED:
+        break
+
+    _,frame = video.read()
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.3, minNeighbors = 7, minSize = (50,50))
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0,255,0), 2)
+
+    imgbytes = cv2.imencode('.png', frame)[1].tobytes()
+    window['-IMAGE-'].update(data = imgbytes)
+
+    window['-TEXT-'].update(len(faces))
+
+window.close()
