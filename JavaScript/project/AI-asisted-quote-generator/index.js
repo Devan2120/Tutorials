6f009d21a3ad97fe27f3424f8e6b9fa4387a89ee
Devananth V <113325565+ep20btech11004@users.noreply.github.com>
2023-08-14 01:09:09 +0530

    Add files via upload

---
+++ JavaScript/project/AI-asisted-quote-generator/index.js
@@ -0,0 +1,11 @@
+import { generateTextAndImage } from "./utils.js"
+
+let name = "Devananth V"
+
+let favoriteActivity = "programming"
+
+let favoritePlace = "beach"
+
+let temperature = 1
+
+generateTextAndImage(name, favoriteActivity, favoritePlace, temperature)
